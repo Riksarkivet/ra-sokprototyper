da411a418808ca88794a89a97a4b54c2faf01d0d
Abbe98 <albin.post@gmail.com>
2022-09-21 17:02:31 +0200

    migrate grid browser to Svelte and make it render an actual grid

---
+++ grid-browser/src/App.svelte
@@ -0,0 +1,107 @@
+<script>
+	import Loader from "./Loader.svelte";
+
+	let items = [];
+	function loadFromManifest(data) {
+		let title;
+		let source;
+		let date;
+		data.metadata.forEach((item) => {
+			if (item.label.sv[0] == "Titel") {
+				title = item.value.none[0];
+			} else if (item.label.sv[0] == "Källhänvisning") {
+				source = item.value.none[0];
+			} else if (item.label.sv[0] == "Datering") {
+				date = item.value.none[0];
+			}
+		});
+
+		data.items.forEach((item) => {
+			if (item.type == "Canvas") {
+				let image = item.id.replace(
+					"/canvas",
+					"/square/200,/0/default.jpg"
+				);
+				let link = item.id
+					.replace("/canvas", "")
+					.replace(
+						"https://lbiiif.riksarkivet.se/arkis!",
+						"https://sok.riksarkivet.se/bildvisning/"
+					);
+				items.push({
+					title: title,
+					source: source,
+					date: date,
+					image: image,
+					link: link,
+				});
+				items = items; // for the Svelte compiler https://svelte.dev/tutorial/updating-arrays-and-objects
+			}
+		});
+	}
+
+	let foundURIs = [];
+	let title;
+	function traverse(url) {
+		fetch(url)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				if (!foundURIs.length) {
+					title = data.label.sv[0];
+					document.title = title;
+				}
+
+				if (data.type == "Manifest") {
+					loadFromManifest(data);
+				}
+
+				foundURIs.push(url);
+
+				for (let item of data.items) {
+					if (!foundURIs.includes(item.id)) {
+						if (item.type == "Manifest") {
+							traverse(item.id);
+						} else if (item.type == "Collection") {
+							traverse(item.id);
+						}
+					}
+				}
+			});
+	}
+
+	let manifest = new URLSearchParams(window.location.search).get("manifest");
+	traverse(manifest);
+</script>
+
+<main>
+	<h1>{title}!</h1>
+	{#if items.length > 0}
+		<div class="container">
+			{#each items as item}
+				<a href={item.link}>
+					<img src={item.image} alt="{item.title}" />
+				</a>
+			{/each}
+		</div>
+	{:else}
+		<Loader />
+	{/if}
+</main>
+
+<style>
+.container {
+	display: grid;
+	grid-template-columns: repeat(8, 1fr);
+}
+
+.container a {
+	overflow: hidden;
+	aspect-ratio: 1;
+}
+
+.container a > img {
+	width: 100%;
+}
+</style>
---
+++ grid-browser/src/Loader.svelte
@@ -0,0 +1,29 @@
+<div class="spinner"></div>
+
+<style>
+    .spinner {
+        display: block;
+        width: 80px;
+        height: 80px;
+        margin: auto;
+    }
+    .spinner:after {
+        content: " ";
+        display: block;
+        width: 64px;
+        height: 64px;
+        margin: 8px;
+        border-radius: 50%;
+        border: 6px solid #00a1deb0;
+        border-color: #00a1deb0 transparent #00a1deb0 transparent;
+        animation: spinner 1.2s linear infinite;
+    }
+    @keyframes spinner {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
