e53ac155ef2246498e38209aa8c44675794b77b3
Abbe98 <albin.post@gmail.com>
2022-10-10 09:19:02 +0200

    make grid row items adaptive based on window width

--- grid-browser/src/App.svelte
+++ grid-browser/src/App.svelte
@@ -201,4 +201,29 @@ nav {
 .container a > img {
 	width: 100%;
 }
+
+/* grid template break points */
+@media (max-width: 1200px) {
+	.container {
+		grid-template-columns: repeat(6, 1fr);
+	}
+}
+
+@media (max-width: 900px) {
+	.container {
+		grid-template-columns: repeat(4, 1fr);
+	}
+}
+
+@media (max-width: 600px) {
+	.container {
+		grid-template-columns: repeat(2, 1fr);
+	}
+}
+
+@media (max-width: 400px) {
+	.container {
+		grid-template-columns: repeat(1, 1fr);
+	}
+}
 </style>
